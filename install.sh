a5ce73cf8a36ae634bd3da73a87b56df1c0060c3
jamesmarc2016 <934396065@qq.com>
2018-12-08 21:55:07 +0800

    t

--- install.sh
+++ install.sh
@@ -331,9 +331,7 @@ nginx_conf_add(){
         }
 		location / {
 		proxy_set_header   X-Real-IP \$remote_addr;
-		proxy_set_header   Host      \$http_host;
-		proxy_pass         https://yun.cloudtang.cf;
-
+		proxy_pass         http://yun.cloudtang.cf;
 		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 		proxy_set_header X-Forwarded-Proto \$scheme;
 	}
